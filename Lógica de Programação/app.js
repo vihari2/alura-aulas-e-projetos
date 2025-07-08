4f30725300d4cf39863c2f4f2f0ee90939a4d9ec
vitoria <freitasvitoria712@gmail.com>
2025-07-07 21:45:59 -0300

    [add] atividades

---
+++ Lógica de Programação/app.js
@@ -0,0 +1,43 @@
+// alert('Boas vindas ao jogo do número secreto!');
+// let numeroSecreto  = 29;
+// chute = prompt('Digite um numero entre 1 e 100: '); 
+// if (numeroSecreto  == chute) {
+//     console.log('Parabéns, você acertou o número secreto! ' + numeroSecreto);
+//      } else {
+//         alert('Que pena, você errou. o numero secreto é: ' + numeroSecreto + ' O seu chute foi: ' + chute);
+//         console.log(chute);
+// }
+
+alert('Boas vindas ao nosso site!')
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let SaldoDisponivel = 1000;
+let mensagemdeErro = 'Erro! Preencha todos os campos';
+alert('Erro, preencha todos os campos');
+alert(mensagemdeErro); 
+
+nomeFulano = prompt('Qual é seu nome?'); 
+idadeFulano = prompt('Qual é sua idade?'); 
+
+if (idadeFulano >= 18) {
+    alert("Parabéns, pode tirar a habilitação")
+}  else {
+    alert('Não pode tirar a habilitação ainda, você tem apenas ' + idadeFulano + ' anos');
+}
+
+
+
+// Operadores de comparação em JavaScript
+// = declaração de variáveis
+// == comparação de valores
+// === comparação de valores e tipos
+// != comparação de valores, mas não tipos
+// !== comparação de valores e tipos    
+// > maior que
+// < menor que
+// >= maior ou igual a
+// <= menor ou igual a
+// && operador lógico E
+// || operador lógico OU
+// ! operador lógico NÃO   
