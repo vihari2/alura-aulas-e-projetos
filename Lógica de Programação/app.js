b9983bc1b520a18fc0e8923821cbcbef15f033d0
vitoria <freitasvitoria712@gmail.com>
2025-07-11 01:09:05 -0300

    [add]

--- Lógica de Programação/app.js
+++ Lógica de Programação/app.js
@@ -131,3 +131,32 @@ let contador = 0;
 // && operador lógico E
 // || operador lógico OU
 // ! operador lógico NÃO   
+
+// AND (&&)
+// O operador AND, representado pelos símbolos "&&", é utilizado para combinar duas condições e avaliar se ambas são verdadeiras. 
+// Se ambas as condições forem verdadeiras, o resultado será… verdadeiro. Caso contrário, logicamente será falso. Por exemplo:
+
+    let idadee = 25;
+    let possuiCarteira = true;
+
+    // se idade é maior que 18 e possui carteira…
+    if (idadee > 18 && possuiCarteira) {
+    console.log("Pode dirigir!");
+    } else {
+    console.log("Não pode dirigir.");
+    }
+
+
+// OR (||)
+// O operador OR, representado pelos símbolos "||", é usado para verificar se pelo menos uma das condições é verdadeira. 
+// Se uma das condições for verdadeira, o resultado será verdadeiro. Se ambas forem falsas, o resultado será falso. Veja um exemplo:
+
+    let temMaça = false;
+    let temBanana = true;
+
+    // se tem maça ou tem banana…
+    if (temMaça || temBanana) {
+    console.log("Você tem frutas!");
+    } else {
+    console.log("Não tem frutas.");
+    }
