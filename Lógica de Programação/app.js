903a15de72905a1fa0031966c879ebad3b970107
vitoria <freitasvitoria712@gmail.com>
2025-07-25 17:57:57 -0300

    [add] desafio

--- Lógica de Programação/app.js
+++ Lógica de Programação/app.js
@@ -1,12 +1,13 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto  = parseInt(Math.random() * 100 + 1); //parseInt(Math.random() * 100 + 1) garante que sempre teremos um número inteiro entre 1 e 100 (inclusive).
+let numeroMaximo = 5000;
+let numeroSecreto  = parseInt(Math.random() * numeroMaximo + 1); //parseInt(Math.random() * 100 + 1) garante que sempre teremos um número inteiro entre 1 e 100 (inclusive).
 let chute;
 let tentativas = 1;
 
 //loop
 //enquanto o chute não for igual ao n.s;
 while( chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100')
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     if(chute == numeroSecreto) {
         break;   
     } else {
